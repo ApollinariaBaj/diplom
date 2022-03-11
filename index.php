bd4c7ed4a71433841821b8cbda322a79fc46e53e
ApollinariaBaj <polina.bajenowa@yandex.ru>
2022-03-11 04:43:16 +0300

    upd index.php and css

--- index.php
+++ index.php
@@ -1,18 +1,41 @@
-<html>
-<title> Необходима авторизация</title>
-<head>
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>
 
+<html xmlns='http://www.w3.org/1999/xhtml'>
+
+<head>
+    <meta http-equiv='content-type' content='text/html; charset=windows-1251'/>
+
+    <title> Необходима авторизация</title>
+
+    <link rel="stylesheet" type="text/css" href="js/jquery-ui-1.7.2.custom.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" href="css/jquery.treeview.css"/>
+    <script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+    <script type="text/javascript" src="js/jquery-ui-1.7.2.custom.min.js"></script>
+    <script type="text/javascript" src="js/ui.datepicker-uk.js"></script>
+    <script type="text/javascript" src="js/jquery.validate.min.js"></script>
+    <script type="text/javascript" src="js/forma.js"></script>
+    <script src="js/jquery.treeview.js" type="text/javascript"></script>
+
+    <script type="text/javascript">
+        $(function () {
+            $("#tree").treeview({
+                collapsed: false,
+                animated: "medium",
+                control: "#sidetreecontrol",
+                persist: "location"
+            });
+        })
+    </script>
+</head>
 <body>
 <br>
 <br>
-
-<div style="text-align: center;">
-    <link rel='stylesheet' type='text/css' href='css/index.css'/>
-    <div id='main' class='noifixpng'>
-    </div>
-    <div style="text-align: center;">
-
-        <div style="text-align: center;">
+<div id='main'>
+    <div class='noifixpng'></div>
+    <div>
+        <div>
             <table align="center" cellspacing="2" cellpadding="2" border="0" width="600" bgcolor="#99FF66">
                 <tr>
                     <td colspan="3" align="center" class="ppp">
@@ -28,8 +51,10 @@
                             ?></h2>
                         <table width="600">
                             <tr>
-                                <td width="150" align="center"><img src="image/user.png" height="80" width="80"></td>
-                                <td width="300" align="center"><h2>Необходима авторизация</h2> <br> <a href="reg.php">Регистрация</a><br>
+                                <td width="150" align="center"><img src="image/user.png" height="80" width="80">
+                                </td>
+                                <td width="300" align="center"><h2>Необходима авторизация</h2> <br> <a
+                                            href="reg.php">Регистрация</a><br>
                                 </td>
                                 <td width="150" align="center"><img src="image/cup.png" height="80" width="80"></td>
                             </tr>
@@ -38,7 +63,7 @@
                 </tr>
                 <tr>
                     <td bgcolor="#ffffcc" align="center">
-                        <center>Введите логин и пароль
+                        <div>Введите логин и пароль</div>
                     </td>
                 </tr>
                 <tr>
@@ -57,10 +82,10 @@
                                 <tr>
                                     <td colspan=2>
                                         <br>
-                                        <center>
+                                        <div style="text-align: center;">
                                             <input type="submit" name="parol" width="99" height="33" border="0"
                                                    value="Вход">
-
+                                        </div>
                                     </td>
                                 </tr>
                             </table>
@@ -69,14 +94,14 @@
                 </tr>
                 <tr>
                     <td class="tr">
-                        <div style="text-align: center;"><a href="admin/index.php">Вход для администратора</a><br></div>
+                        <div style="text-align: center;"><a href="admin/index.php">Вход для администратора</a><br>
+                        </div>
                     </td>
                 </tr>
             </table>
         </div>
     </div>
 </div>
-
 </body>
 
 </html>
