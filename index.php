0b21e1b79ce9a789055619dee8fcedcc099acb6e
ApollinariaBaj <polina.bajenowa@yandex.ru>
2022-03-20 18:38:11 +0300

    users page

--- index.php
+++ index.php
@@ -13,10 +13,9 @@ if (Users::isAuthorized()) {
     <meta http-equiv='content-type' content='text/html; charset=windows-1251'/>
 
     <title>Необходима авторизация</title>
-
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel='stylesheet' type='text/css' href='/css/index.css'/>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
 </head>
 <body>
 <div class='container text-center'>
--- users.php
+++ users.php
@@ -17,34 +17,54 @@ $users = (new Users())->getUsers();
     <meta http-equiv='content-type' content='text/html; charset=windows-1251'/>
 
     <title>Пользователи</title>
-
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel='stylesheet' type='text/css' href='/css/index.css'/>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
 </head>
 <body>
 <div class='container-fluid'>
-    <div class="table-responsive text-nowrap">
-        <!--Table-->
-        <table class="table table-striped text-center">
+    <div class="container mb-2 mt-2">
+        <div class="btn-toolbar justify-content-between" role="toolbar">
+            <div class="btn-group mr-2" role="group">
+                <button type="button" class="btn btn-primary">Добавить пользователя</button>
+            </div>
+            <div class="input-group">
+                <input type="search" class="form-control rounded" placeholder="Поиск" aria-label="Поиск"
+                       aria-describedby="search-addon"/>
+                <button type="button" class="btn btn-primary" id="search-addon">Поиск</button>
+            </div>
+        </div>
+    </div>
+    <!--Table-->
+    <div class="pre-scrollable">
+        <table class="table table-hover table-bordered custom-table"
+               cellspacing="0">
             <thead>
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">Логин</th>
-                <th scope="col">Администратор</th>
-                <th colspan="2" scope="col">Действия</th>
+                <th scope="col" class="th-sm text-center">#</th>
+                <th scope="col" class="th-sm text-center">Логин</th>
+                <th scope="col" class="th-sm text-center">Администратор</th>
+                <th colspan="2" scope="col" class="th-sm text-center">Действия</th>
             </tr>
             </thead>
             <tbody>
             <? foreach ($users as $key => $user) { ?>
                 <tr>
-                    <th scope="row"><?= $key; ?></th>
-                    <td><?= $user->login ?></td>
-                    <td><?= $user->is_admin ?></td>
-                    <td>
+                    <th scope="row" class="text-center"><?= $key + 1; ?></th>
+                    <td class="text-center"><?= $user->login ?></td>
+                    <td class="text-center"><?= $user->is_admin ?
+                            '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+  <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+</svg>'
+                            : '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"/>
+  <path fill-rule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"/>
+</svg>' ?></td>
+                    <td class="text-center">
                         <button class="btn btn-primary" type="submit">Редактировать</button>
                     </td>
-                    <td>
+                    <td class="text-center">
                         <button class="btn btn-danger" type="submit">Удалить</button>
                     </td>
                 </tr>
